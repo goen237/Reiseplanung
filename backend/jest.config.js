d0dea50576b5f57568e3a887c6115020111cf9f2
Goerres <goerresvalere.nintidemkemlikeu@stud.h-da.de>
2025-05-17 23:14:44 +0200

    .

---
+++ backend/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // setupFilesAfterEnv: [
+  //   "<rootDir>/jest.setup.js"
+  // ],
+  testMatch: [
+    "**/tests/**/*.test.ts"
+  ],
+  transform: {
+    "^.+\.tsx?$": ["ts-jest",{}],
+  },
+};
